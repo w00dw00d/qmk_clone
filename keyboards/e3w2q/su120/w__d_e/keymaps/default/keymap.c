5ecf3be1abcaf5573b8ad1c27b91041fb5b40631
w00dw00d <w__d@outlook.com>
2022-08-06 00:36:08 +0900

    日本語変換・@・ウィンドウ操作・HoneEnd等

--- keyboards/e3w2q/su120/w__d_e/keymaps/default/keymap.c
+++ keyboards/e3w2q/su120/w__d_e/keymaps/default/keymap.c
@@ -81,39 +81,43 @@ const key_override_t **key_overrides = (const key_override_t *[]){
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
-  MHENK,
-  AT_TB,
   AT_TS,
   AT_PW,
+  SP_SF,
+  SP_HK,
+  SP_MW,
+  SP_AT,
 };
 
 static bool is_timer = false;
 static bool is_not_pressed_key = false;
+static bool is_sp_hk_pressed = false;
 
 #define _QWERTY 0
 #define _LOWER 1
 // #define _RAISE 2
 
-// #define MO_LO MO(_LOWER)
+// #define ZH_TG A(JP_ZKHK)
+#define MO_LO MO(_LOWER)
 // #define MO_RA MO(_RAISE)
 
 // #define CL_PU C(KC_PGUP)
 // #define CL_PD C(KC_PGDN)
-
+// KC_MHEN KC_HENK
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT(
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_BSPC, \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,  KC_BSLS, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,  \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT, \
-    KC_LGUI, KC_LALT, KC_HENK,          MHENK,   KC_SPC,  LOWER,                     KC_LBRC, KC_RBRC, KC_APP   \
+    KC_LGUI, KC_LALT, SP_AT,            SP_SF,   KC_SPC,  MO_LO,                     KC_LBRC, KC_RBRC, KC_APP   \
   ),
   [_LOWER] = LAYOUT(
     RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
-    KC_INS,  AT_TS,   _______, AT_PW,   _______, _______, _______, _______, KC_UP,   KC_PGUP, KC_PGDN, _______, KC_GRV, \
-    _______, _______, _______, _______, _______, KC_HOME, KC_END,  KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, \
+    KC_INS,  _______, _______, _______, _______, _______, _______, _______, KC_UP,   KC_PGUP, KC_PGDN, _______, KC_GRV, \
+    _______, _______, _______, _______, SP_MW,   _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______,          _______, KC_DEL,  _______,                   _______, _______, _______  \
+    _______, _______, _______,          SP_HK,   KC_DEL,  _______,                   AT_TS,   AT_PW,   _______  \
   ),
 };
 
@@ -156,7 +160,7 @@ void keyboard_post_init_user(void) {
 
 void jamming(void) {
   tap_code(KC_LEFT);
-  wait_ms(1000);
+  wait_ms(500);
 }
 
 void matrix_scan_user(void) {
@@ -167,31 +171,89 @@ void matrix_scan_user(void) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case LOWER:
+    case SP_AT:
       if (record->event.pressed) {
-        is_not_pressed_key = true;
-        layer_on(_LOWER);
-      } else {
-        if (is_not_pressed_key) {
-          tap_code(KC_HENK);
-        }
-        is_not_pressed_key = false;
-        layer_off(_LOWER);
+        tap_code(KC_MHEN);
+        tap_code(JP_AT);
       }
+      is_not_pressed_key = false;
       break;
 
-    case MHENK:
+    case SP_MW:
+      if (record->event.pressed) {
+        register_code(KC_LALT);
+        tap_code(KC_TAB);
+      } else {
+        unregister_code(KC_LALT);
+      }
+      is_not_pressed_key = false;
+      break;
+
+    case SP_SF:
       if (record->event.pressed) {
         is_not_pressed_key = true;
-        register_code(KC_LSFT);
+        register_code(KC_RSFT);
       } else {
-        unregister_code(KC_LSFT);
+        unregister_code(KC_RSFT);
         if (is_not_pressed_key) {
           tap_code(KC_MHEN);
         }
         is_not_pressed_key = false;
       }
       break;
+
+    case SP_HK:
+      if (record->event.pressed) {
+        is_not_pressed_key = true;
+        is_sp_hk_pressed = true;
+      } else {
+        if (is_not_pressed_key) {
+          tap_code(KC_HENK);
+        }
+        is_sp_hk_pressed = false;
+        is_not_pressed_key = false;
+      }
+      break;
+
+    case KC_LEFT:
+      if (! is_sp_hk_pressed) {
+        break;
+      }
+      if (record->event.pressed) {
+        tap_code(KC_HOME);
+        is_not_pressed_key = false;
+        return false;
+      }
+
+    case KC_RGHT:
+      if (! is_sp_hk_pressed) {
+        break;
+      }
+      if (record->event.pressed) {
+        tap_code(KC_END);
+        is_not_pressed_key = false;
+        return false;
+      }
+
+    case KC_UP:
+      if (! is_sp_hk_pressed) {
+        break;
+      }
+      if (record->event.pressed) {
+        tap_code(KC_PGUP);
+        is_not_pressed_key = false;
+        return false;
+      }
+
+    case KC_DOWN:
+      if (! is_sp_hk_pressed) {
+        break;
+      }
+      if (record->event.pressed) {
+        tap_code(KC_PGDN);
+        is_not_pressed_key = false;
+        return false;
+      }
 
     case KC_LCTL:
       if (record->event.pressed) {
@@ -212,6 +274,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         SEND_STRING("rkUsers37");
       }
       break;
+
     default:
       if (record->event.pressed) {
         is_not_pressed_key = false;
