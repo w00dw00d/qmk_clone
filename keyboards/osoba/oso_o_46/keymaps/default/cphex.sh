b75713154c7e4f9298fa21699c4db850761590f5
w__d <w__d@outlook.com>
2022-11-05 23:49:21 +0900

    mac対応

--- keyboards/osoba/oso_o_46/keymaps/default/cphex.sh
+++ keyboards/osoba/oso_o_46/keymaps/default/cphex.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp /home/w/qmk_firmware/.build/osoba_oso_o_46_default.hex /mnt/c/02Work/keyboard/
-echo Complete! 
+cp ~/Documents/develop/qmk_clone/.build/osoba_oso_o_46_default.hex ~/Documents/tmp/
+echo Complete!
